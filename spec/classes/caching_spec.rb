cd6cdd440d23cd00838020efec5049f5cec01193
nick-markowski <nmarkowski@keywcorp.com>
2016-04-15 14:05:04 +0000

    (SIMP-969) Named work-around for startup in chroot
    - Created work-around for https://bugzilla.redhat.com/show_bug.cgi?id=1278082
    - Users can modify the chroot path in named-chroot.service
    - Asserted service, chroot, and non-chroot as private classes
    
    SIMP-969 #close
    
    Change-Id: Idf53c441d9d3053b9c68d239105e5a942420f621

--- spec/classes/caching_spec.rb
+++ spec/classes/caching_spec.rb
@@ -7,10 +7,18 @@ describe 'named::caching' do
         let(:facts){ facts }
         it { is_expected.to create_class('named::caching') }
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_package('bind-chroot') }
-        it { is_expected.to create_file('/etc/named.conf') }
         it { is_expected.to create_file('/etc/named.rfc1912.zones') }
         it { is_expected.to contain_service('named').with_ensure('running') }
+        context 'with selinux enabled' do
+          it { is_expected.to create_file('/etc/named.conf') }
+        end
+        context 'with selinux disabled' do
+          let(:facts) { facts.merge( { :selinux_enforced => false } ) }  
+          let(:params) {{ :chroot_path => '/var/named/chroot' }}
+          it { is_expected.to contain_package('bind-chroot') }
+          it { is_expected.to create_file('/etc/named.conf').with_ensure('symlink') }
+          it { is_expected.to create_file('/var/named/chroot/etc/named.conf')  }
+        end
       end
     end
   end
