f051001a4d1536a0f1bc22a59d323146892ff1d9
Steven Pritchard <steve@sicura.us>
2025-05-06 09:39:36 -0500

    Clean up for rubocop (#88)

--- spec/acceptance/suites/default/00_caching_spec.rb
+++ spec/acceptance/suites/default/00_caching_spec.rb
@@ -3,29 +3,28 @@ require 'spec_helper_acceptance'
 test_name 'named::caching'
 
 describe 'named::caching' do
-  let(:manifest) {
+  let(:manifest) do
     <<-EOS
       include 'named::caching'
 
       named::caching::forwarders { '8.8.8.8': }
     EOS
-  }
+  end
 
   hosts.each do |host|
     # Need this for the 'host' command
     host.install_package('bind-utils')
 
     context 'with internet connection' do
-
-      it 'should work with no errors' do
-        apply_manifest_on(host, manifest, :catch_failures => true)
+      it 'works with no errors' do
+        apply_manifest_on(host, manifest, catch_failures: true)
       end
 
-      it 'should be idempotent' do
-        apply_manifest_on(host, manifest, :catch_failures => true)
+      it 'is idempotent' do
+        apply_manifest_on(host, manifest, catch_failures: true, cactch_changes: true)
       end
 
-      it 'should be able to lookup www.google.com from itself' do
+      it 'is able to lookup www.google.com from itself' do
         on(host, 'echo -e "nameserver 127.0.0.1\nsearch `facter domain`" > /etc/resolv.conf')
         on(host, 'host www.google.com')
       end
--- spec/classes/caching/hints_spec.rb
+++ spec/classes/caching/hints_spec.rb
@@ -7,7 +7,7 @@ describe 'named::caching::hints' do
         let(:facts) { facts }
 
         it { is_expected.to create_class('named::caching::hints') }
-        it { is_expected.to create_file('/var/named/chroot/var/named/named.ca').with_content(/test_content/) }
+        it { is_expected.to create_file('/var/named/chroot/var/named/named.ca').with_content(%r{test_content}) }
       end
     end
   end
